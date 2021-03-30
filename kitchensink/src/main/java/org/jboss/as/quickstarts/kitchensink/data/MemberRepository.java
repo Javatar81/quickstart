d09a5be4822edb37183d5a06576fb656f355ed31
bschmeling <benjamin.schmeling@gmail.com>
2021-03-30 19:39:12 +0200

    Task1 - Implement check whether user already exists

--- kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/data/MemberRepository.java
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/data/MemberRepository.java
@@ -23,6 +23,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.Optional;
 
 import org.jboss.as.quickstarts.kitchensink.model.Member;
 
@@ -45,6 +46,18 @@ public class MemberRepository {
         // criteria.select(member).where(cb.equal(member.get(Member_.email), email));
         criteria.select(member).where(cb.equal(member.get("email"), email));
         return em.createQuery(criteria).getSingleResult();
+    }
+    
+    public Optional<Member> findByNameOrEmail(String name, String email) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> criteria = cb.createQuery(Member.class);
+        Root<Member> member = criteria.from(Member.class);
+        // Swap criteria statements if you would like to try out type-safe criteria queries, a new
+        // feature in JPA 2.0
+        // criteria.select(member).where(cb.equal(member.get(Member_.email), email));
+        criteria.select(member).where(cb.or(cb.equal(member.get("email"), email), cb.equal(member.get("name"), name)));
+        List<Member> resultList = em.createQuery(criteria).getResultList();
+        return Optional.ofNullable(!resultList.isEmpty() ? resultList.get(0) : null);
     }
 
     public List<Member> findAllOrderedByName() {
--- kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/service/MemberRegistration.java
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/service/MemberRegistration.java
@@ -16,12 +16,15 @@
  */
 package org.jboss.as.quickstarts.kitchensink.service;
 
+import org.jboss.as.quickstarts.kitchensink.data.MemberRepository;
 import org.jboss.as.quickstarts.kitchensink.model.Member;
 
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+
+import java.util.Optional;
 import java.util.logging.Logger;
 
 // The @Stateless annotation eliminates the need for manual transaction demarcation
@@ -36,10 +39,19 @@ public class MemberRegistration {
 
     @Inject
     private Event<Member> memberEventSrc;
+    
+    @Inject
+    private MemberRepository memberRepository;
 
     public void register(Member member) throws Exception {
-        log.info("Registering " + member.getName());
-        em.persist(member);
-        memberEventSrc.fire(member);
+    	// Check whether user is already in database
+    	Optional<Member> existingMember = memberRepository.findByNameOrEmail(member.getName(), member.getEmail());
+    	if (existingMember.isPresent()) {
+    		throw new RuntimeException(String.format("User with email '%s' or name '%s' already registered", existingMember.get().getEmail(), existingMember.get().getName()));
+    	} else {    		
+    		log.info("Registering " + member.getName());
+    		em.persist(member);
+    		memberEventSrc.fire(member);
+    	}
     }
 }
